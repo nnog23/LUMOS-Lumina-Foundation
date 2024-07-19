15e08c17709664d9edd6ff4dbd7cb43d0a6b61b5
LesterAnthony <134356297+Lester-Anthony@users.noreply.github.com>
2024-07-19 18:06:06 +0800

    Lei Commit 1
    Currently working on header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
-
+import tailwind from "@astrojs/tailwind";
 dotenv.config();
 
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
